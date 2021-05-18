47630547f63a041e4169692fa054c6eb01634cf9
luxada <72996914+luxada@users.noreply.github.com>
2021-05-18 17:58:28 +0100

    adicionei exercícios

--- 50.c
+++ 50.c
@@ -909,6 +909,70 @@ int removeOneOrd (LInt *l, int x){
 //exercício 57
 
 
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
+void merge (LInt *r, LInt a, LInt b){
+    
+    if(a == NULL){
+        
+      *r = b;  
+      
+      return;
+    } 
+    
+    if(b == NULL){
+        
+      *r = a;  
+      
+      return;
+    } 
+    
+    LInt atual = newLInt(0, NULL);
+    
+    *r = atual;
+    
+    while(a != NULL && b != NULL){
+        
+        if(a->valor < b->valor){
+            
+            atual->prox = a;
+            
+            a = a->prox;
+            
+        } else{
+            
+            atual->prox = b;
+            
+            b = b->prox;
+        }
+        
+        atual = atual->prox;
+    }
+    
+    if(a == NULL) atual->prox = b;
+    
+    else atual->prox = a;
+    
+    atual = *r;
+    
+    *r = atual->prox;
+    
+    free(atual);
+    
+    return;
+ 
+}
+
+//exercício 58
+
 
 
 
