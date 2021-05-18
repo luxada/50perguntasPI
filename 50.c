97705dcfa2c34359a00dc8a32f50035d3bf00c53
luxada <72996914+luxada@users.noreply.github.com>
2021-05-18 19:37:37 +0100

    adicionei exercícios

--- 50.c
+++ 50.c
@@ -974,5 +974,46 @@ void merge (LInt *r, LInt a, LInt b){
 //exercício 58
 
 
+void splitQS (LInt l, int x, LInt *mx, LInt *Mx){
+    
+    LInt atual = l;
+    
+    *mx = newLInt(0, NULL);
+    
+    LInt mxa = *mx;
+    
+    *Mx = newLInt(0, NULL);
+    
+    LInt Mxa = *Mx;
+    
+    while(atual != NULL){
+        
+        if(atual->valor < x){
+            
+            mxa->prox = atual;
+            atual = atual->prox;
+            mxa = mxa->prox;
+            mxa->prox = NULL;
+            
+        } else{
+            
+            Mxa->prox = atual;
+            atual = atual->prox;
+            Mxa = Mxa->prox;
+            Mxa->prox = NULL;
+        }
+    }
+    
+    atual = *mx;
+	*mx = (*mx)->prox;
+	free (atual);
+
+	atual = *Mx;
+	*Mx = (*Mx)->prox;
+	free (atual);
+}
+
+//exercício 59
+
 
 
